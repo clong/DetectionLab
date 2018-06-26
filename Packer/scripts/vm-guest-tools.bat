2719151e73a11031030c95c857a84741acd551c1
Péter Gombos <peter@gombos.no>
2018-06-26 13:23:57 +0200

    Change 7-zip url to https

--- Packer/scripts/vm-guest-tools.bat
+++ Packer/scripts/vm-guest-tools.bat
@@ -1,5 +1,5 @@
 if not exist "C:\Windows\Temp\7z920-x64.msi" (
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://www.7-zip.org/a/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
 )
 msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
 
