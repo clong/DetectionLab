23f0b30dabb5d0f7fcdb660f8f588d67ff0d7a50
Chris Long <clong@cl0ng.com>
2019-03-11 22:37:37 -0700

    Add try/catch to configure-ou.ps1

--- Vagrant/scripts/configure-ou.ps1
+++ Vagrant/scripts/configure-ou.ps1
@@ -1,23 +1,34 @@
 # Purpose: Sets up the Server and Workstations OUs
 Write-Host "Creating Server and Workstation OUs..."
 Write-Host "Creating Servers OU..."
-if (!([ADSI]::Exists("LDAP://OU=Servers,DC=windomain,DC=local")))
-{
-    New-ADOrganizationalUnit -Name "Servers" -Server "dc.windomain.local"
+Write-Host "DEBUG: $env:computername.$env:userdnsdomain"
+try {
+  if (!([ADSI]::Exists("LDAP://OU=Servers,DC=windomain,DC=local")))
+  {
+    New-ADOrganizationalUnit -Name "Servers" -Server "dc.windomain.local"   
+  }
+  else
+  {
+      Write-Host "Servers OU already exists. Moving On."
+  }
+} catch {
+  New-ADOrganizationalUnit -Name "Servers" -Server "dc.windomain.local"}
 }
-else
-{
-    Write-Host "Servers OU already exists. Moving On."
-}
+
 Write-Host "Creating Workstations OU"
-if (!([ADSI]::Exists("LDAP://OU=Workstations,DC=windomain,DC=local")))
-{
+try {
+  if (!([ADSI]::Exists("LDAP://OU=Workstations,DC=windomain,DC=local")))
+  {
     New-ADOrganizationalUnit -Name "Workstations" -Server "dc.windomain.local"
-}
-else
-{
+  }
+  else
+  {
     Write-Host "Workstations OU already exists. Moving On."
+  }
+} catch { 
+  New-ADOrganizationalUnit -Name "Workstations" -Server "dc.windomain.local"
 }
+
 # Sysprep breaks auto-login. Let's restore it here:
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "vagrant"
