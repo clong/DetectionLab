0147bad8d25619b7ba3c5e97e00ead17fa1d1e86
Chris Long <clong@cl0ng.com>
2019-05-10 17:13:54 -0700

    TLS1.2 support for vm-guest-tools.bat

--- Packer/scripts/vm-guest-tools.bat
+++ Packer/scripts/vm-guest-tools.bat
@@ -1,3 +1,4 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 if not exist "C:\Windows\Temp\7z1900-x64.msi" (
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.7-zip.org/a/7z1900-x64.msi', 'C:\Windows\Temp\7z1900-x64.msi')" <NUL
 )
