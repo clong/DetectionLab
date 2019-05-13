82976fd26596cee5a9c02308b765a774f26b4367
Chris Long <clong@cl0ng.com>
2019-05-13 10:14:30 -0700

    Add Packer 1.4.0 workaround for ultradefrag
    Attempting to address issue #259

--- Packer/scripts/compact.bat
+++ Packer/scripts/compact.bat
@@ -27,6 +27,6 @@ rmdir /S /Q C:\Windows\SoftwareDistribution\Download
 mkdir C:\Windows\SoftwareDistribution\Download
 net start wuauserv
 
-cmd /c C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
+start /wait C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
 cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
 cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
