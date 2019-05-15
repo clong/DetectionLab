0f8f230d813b487548ed977951f45140bb5270d9
chiggins <chris.higgins@mandiant.com>
2019-05-15 15:18:45 -0500

    Fixes #284
    Changes the AWS profile to be a user-supplied variable, opposed to
    statically being just `terraform`.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   shared_credentials_file = "${var.shared_credentials_file}"
   region = "${var.region}"
-  profile = "terraform"
+  profile = "${var.profile}"
 }
 
 # Create a VPC to launch our instances into
