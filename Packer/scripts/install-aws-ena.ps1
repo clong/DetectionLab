1b64af6e0b4805290a4858876387d6299230a438
Chris Long <clong@cl0ng.com>
2020-05-07 14:18:20 -0700

    Fixing path

--- Packer/scripts/install-aws-ena.ps1
+++ Packer/scripts/install-aws-ena.ps1
@@ -3,7 +3,7 @@
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
   $url="https://s3.amazonaws.com/ec2-windows-drivers-downloads/ENA/Latest/AwsEnaNetworkDriver.zip"
   (New-Object System.Net.WebClient).DownloadFile($url, "$env:TEMP\AwsEnaNetworkDriver.zip")
-  Expand-Archive -Path $env:TEMP\AwsEnaNetworkDriver.zip -DestinationPath $env:TEMP -Force
+  Expand-Archive -Path $env:TEMP\AwsEnaNetworkDriver.zip -DestinationPath $env:TEMP\AwsEnaNetworkDriver -Force
   . $env:TEMP\AwsEnaNetworkDriver\install.ps1
 
   rm $env:TEMP\AwsEnaNetworkDriver.zip
