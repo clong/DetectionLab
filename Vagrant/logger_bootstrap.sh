000f3dc6f64b097f5ac74e5aab59fa99619d541b
snlppy <snippy22@gmail.com>
2020-11-25 12:18:18 +1000

    Update logger_bootstrap.sh
    removed ping

--- Vagrant/logger_bootstrap.sh
+++ Vagrant/logger_bootstrap.sh
@@ -545,7 +545,7 @@ postinstall_tasks() {
   echo export PATH="$PATH:/opt/splunk/bin:/opt/zeek/bin" >>~/.bashrc
   echo "export SPLUNK_HOME=/opt/splunk" >>~/.bashrc
   # Ping DetectionLab server for usage statistics
-  curl -s -A "DetectionLab-logger" "https:/ping.detectionlab.network/logger" || echo "Unable to connect to ping.detectionlab.network"
+  #curl -s -A "DetectionLab-logger" "https:/ping.detectionlab.network/logger" || echo "Unable to connect to ping.detectionlab.network"
 }
 
 main() {
