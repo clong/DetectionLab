70e6d0091d079b31e5a1158d1743487ace391ab2
Chris Long <clong@cl0ng.com>
2021-10-27 11:06:09 -0700

    fix azure image

--- Azure/Terraform/main.tf
+++ Azure/Terraform/main.tf
@@ -230,8 +230,8 @@ resource "azurerm_virtual_machine" "logger" {
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 
