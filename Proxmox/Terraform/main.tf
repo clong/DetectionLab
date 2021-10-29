58a14f3cdddbbec21e929dd34d66643acb53ac81
Ludek Suk <ludek.suk@gmail.com>
2021-10-29 22:27:50 +0200

    Added Terraform and Ansible

---
+++ Proxmox/Terraform/main.tf
@@ -0,0 +1,165 @@
+provider "proxmox" {
+    pm_api_url = var.proxmox_api_url
+    pm_user = var.proxmox_api_user
+    pm_password = var.proxmox_api_password
+    pm_tls_insecure = true
+    pm_parallel = 4
+}
+
+resource "proxmox_vm_qemu" "logger" {
+  name = "logger"
+  target_node = var.proxmox_node
+  clone = "Ubuntu1804"
+  full_clone = true
+  desc = "logger"
+  cores = "2"
+  sockets = "1"
+  cpu = "host"
+  memory = "4096"
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent = 1
+
+  disk {
+    size = "64G"
+    type = "scsi"
+    storage = var.vm_disk
+    cache = "writeback"
+    format = "raw"
+    discard = var.vm_disk_discard
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.vm_network
+    macaddr = "00:50:56:a3:b1:c2"
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.hostonly_network
+    macaddr = "00:50:56:a3:b1:c4"
+  }
+
+    connection {
+      host = self.ssh_host
+      type = "ssh"
+      user = "vagrant"
+      password = "vagrant"
+    }
+
+    provisioner "remote-exec" {
+    inline = [
+      "sudo ifconfig eth0 up && echo 'eth0 up' || echo 'unable to bring eth0 interface up",
+      "sudo ifconfig eth1 up && echo 'eth1 up' || echo 'unable to bring eth1 interface up"
+    ]
+  }
+}
+
+resource "proxmox_vm_qemu" "dc" {
+  name = "dc"
+  target_node = var.proxmox_node
+  clone = "WindowsServer2016"
+  full_clone = true
+  desc = "dc"
+  cores = "2"
+  sockets = "1"
+  cpu = "host"
+  memory = "4096"
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent = 1
+
+  disk {
+    size = "64G"
+    type = "scsi"
+    storage = var.vm_disk
+    cache = "writeback"
+    format = "raw"
+    discard = var.vm_disk_discard
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.vm_network
+    macaddr = "00:50:56:a1:b1:c2"
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.hostonly_network
+    macaddr = "00:50:56:a1:b1:c4"
+  }
+}
+
+resource "proxmox_vm_qemu" "wef" {
+  name = "wef"
+  target_node = var.proxmox_node
+  clone = "WindowsServer2016"
+  full_clone = true
+  desc = "wef"
+  cores = "2"
+  sockets = "1"
+  cpu = "host"
+  memory = "2048"
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent = 1
+
+  disk {
+    size = "64G"
+    type = "scsi"
+    storage = var.vm_disk
+    cache = "writeback"
+    format = "raw"
+    discard = var.vm_disk_discard
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.vm_network
+    macaddr = "00:50:56:a1:b2:c2"
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.hostonly_network
+    macaddr = "00:50:56:a1:b4:c4"
+  }
+}
+
+resource "proxmox_vm_qemu" "win10" {
+  name = "win10"
+  target_node = var.proxmox_node
+  clone = "Windows10"
+  full_clone = true
+  desc = "win10"
+  cores = "2"
+  sockets = "1"
+  cpu = "host"
+  memory = "2048"
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent = 1
+
+  disk {
+    size = "64G"
+    type = "scsi"
+    storage = var.vm_disk
+    cache = "writeback"
+    format = "raw"
+    discard = var.vm_disk_discard
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.vm_network
+    macaddr = "00:50:56:a2:b1:c2"
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.hostonly_network
+    macaddr = "00:50:56:a2:b1:c4"
+  }
+}
---
+++ Proxmox/Terraform/modules/exchange/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "2.9.0"
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "exchange" {
+  name = "exchange"
+  target_node = var.proxmox_node
+  clone = "WindowsServer2016"
+  full_clone = true
+  desc = "exchange"
+  cores = "4"
+  sockets = "1"
+  cpu = "host"
+  memory = "8192"
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  agent = 1
+
+  disk {
+    size = "64G"
+    type = "scsi"
+    storage = var.vm_disk
+    cache = "writeback"
+    format = "raw"
+    discard = var.vm_disk_discard
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.vm_network
+    macaddr = "00:50:56:a1:b2:c5"
+  }
+
+  network {
+    model = "virtio"
+    bridge = var.hostonly_network
+    macaddr = "00:50:56:a1:b4:c5"
+  }
+}
