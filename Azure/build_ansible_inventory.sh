49a40cc7b44f7c6f23d31282565ef615eb1b14b3
Chris Long <clong@cl0ng.com>
2021-10-29 21:25:12 -0700

    Address #731
    Fixes https://github.com/clong/DetectionLab/issues/731

--- Azure/build_ansible_inventory.sh
+++ Azure/build_ansible_inventory.sh
@@ -21,10 +21,10 @@ fi
 cd ./Terraform || exit 1
 TF_OUTPUT=$(terraform output)
 
-DC_IP=$(echo "$TF_OUTPUT" | grep -E -o "dc_public_ip = ([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d '=' -f 2 | tr -d ' ')
-WEF_IP=$(echo "$TF_OUTPUT" | grep -E -o "wef_public_ip = ([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d '=' -f 2 | tr -d ' ')
-WIN10_IP=$(echo "$TF_OUTPUT" | grep -E -o "win10_public_ip = ([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d '=' -f 2 | tr -d ' ')
-EXCHANGE_IP=$(echo "$TF_OUTPUT" | grep -E -o "exchange_public_ip = ([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d '=' -f 2 | tr -d ' ')
+DC_IP=$(echo "$TF_OUTPUT" | grep -E -o "dc_public_ip = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"" | cut -d '=' -f 2 | tr -d ' ' | tr -d '"')
+WEF_IP=$(echo "$TF_OUTPUT" | grep -E -o "wef_public_ip = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"" | cut -d '=' -f 2 | tr -d ' ' | tr -d '"')
+WIN10_IP=$(echo "$TF_OUTPUT" | grep -E -o "win10_public_ip = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"" | cut -d '=' -f 2 | tr -d ' ' | tr -d '"')
+EXCHANGE_IP=$(echo "$TF_OUTPUT" | grep -E -o "exchange_public_ip = \"([0-9]{1,3}[\.]){3}[0-9]{1,3}\"" | cut -d '=' -f 2 | tr -d ' ' | tr -d '"')
 
 # Don't update unless there's default values in inventory.yml
 GREP_COUNT=$(grep -E -c 'x\.x\.x\.x|y\.y\.y\.y|z\.z\.z\.z' ../Ansible/inventory.yml)
@@ -38,7 +38,7 @@ fi
 echo "Replacing the default values in DetectionLab/Azure/Ansible/inventory.yml..."
 sed -i.bak "s/x.x.x.x/$DC_IP/g; s/y.y.y.y/$WEF_IP/g; s/z.z.z.z/$WIN10_IP/g" ../Ansible/inventory.yml
 
-if [ ! -e "$EXCHANGE_IP" ]; then
+if [ ! -z "$EXCHANGE_IP" ]; then
   echo "Exchange server found! Adding the IP to the Ansible inventory..."
   sed -i.bak "s/# v.v.v.v/$EXCHANGE_IP/g" ../Ansible/inventory.yml
 fi
