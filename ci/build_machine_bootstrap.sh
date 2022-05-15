a2f7602482c22c0137fdec4aec68bffae94f4798
Chris Long <clong@cl0ng.com>
2022-05-14 23:43:27 -0700

    io redirect

--- ci/build_machine_bootstrap.sh
+++ ci/build_machine_bootstrap.sh
@@ -120,7 +120,7 @@ main() {
   # Build and Test Vagrant hosts
   cd Vagrant || exit 1
   build_vagrant_hosts
-  /bin/bash "$DL_DIR/Vagrant/post_build_checks.sh" > $DL_DIR/Vagrant/post_build.log
+  /bin/bash "$DL_DIR/Vagrant/post_build_checks.sh" &> $DL_DIR/Vagrant/post_build.log
   exit 0
 }
 main
