f8b9976dcc960fff6fdf493ceed3af5cddcb6157
Chris Long <clong@cl0ng.com>
2022-08-21 14:34:29 -0700

    Dev branch for testing hostname change and wait for vmtools

---
+++ Vagrant/scripts/wait-for-vm-tools.ps1
@@ -0,0 +1,30 @@
+# Purpose: Prevents the host from rebooting until VMware Tools or VBox Guest Additions is detected running
+
+$tries=1; 
+While ($tries -lt 10) { 
+    $vmware_service = Get-Service -Name VMTools -ErrorAction SilentlyContinue
+    $vbox_service = Get-Service -Name VBoxService -ErrorAction SilentlyContinue
+    If(($vmware_service.length -eq 0) -and ($vbox_service.length -eq 0)) {
+        Write-Host "Waiting for VM guest tools to be installed"
+        Start-Sleep 10
+    } Else {
+        If ($vbox_service.length -gt 0) {
+          Write-Host "Found Virtualbox Guest Additions!"
+          $service = VBoxService
+        }
+        If ($vmware_service.length -gt 0) {
+          Write-Host "Found VMware Tools!"
+          $service = VMTools
+        }
+        If((get-service -name $service).Status -ne "Running") {
+            Write-Host "Waiting for the service to start" 
+            Start-Sleep 5
+        } Else {
+            Write-Host "$service started successfully!"
+        }
+    }
+    $tries+=1
+}
+
+
+
